ea9780f656033a01f1ee6e9b6d23cabb44de18af
ritikdevelop <hrithikrana216@gmail.com>
2026-03-17 10:57:05 +0530

    feat: Establish initial application architecture with numerous screens and navigation, and migrate from TypeScript to JavaScript.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|react-native-vector-icons|@react-navigation|@react-native-vector-icons|react-native-svg)'
+  ],
+  watchPathIgnorePatterns: [
+    'android(/.*)?\\\\.cxx',
+    'android(/.*)?\\\\build',
+    'ios/build',
+    '\\.cxx(/.*)?',
+    '\\\\.cxx(/.*)?',
+    'build(/.*)?',
+  ],
 };
