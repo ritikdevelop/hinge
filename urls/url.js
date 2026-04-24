83616089abb93306297f2c3b6d1e6ffded0fda19
ritikdevelop <hrithikrana216@gmail.com>
2026-04-24 10:40:59 +0530

    feat: Add development URL configuration for various environments

---
+++ urls/url.js
@@ -0,0 +1,6 @@
+// For development:
+// - Android emulator: http://10.0.2.2:4000
+// - Physical device or iOS simulator: http://<YOUR_MACHINE_IP>:4000
+// - Web/Development: http://localhost:4000
+
+export const BASE_URL = 'http://192.168.137.1:4000'; // Change this to your IP if using a physical device or iOS simulator
